4c7de42d63bbc0a93f4f83352c5faa56f848bc5b
Gregory Maynard <gregory.maynard@valtech.fr>
2012-07-19 15:56:42 +0200

    Init

---
+++ src/SchedulerTask/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Quartz;
+using Quartz.Impl;
+
+namespace SchedulerTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // construct a scheduler factory
+            ISchedulerFactory schedFact = new StdSchedulerFactory();
+
+            // get a scheduler
+            IScheduler sched = schedFact.GetScheduler();
+            sched.Start();
+
+            IJobDetail job = JobBuilder.Create<SchedulerJob>()
+             .WithIdentity("job1", "group1")
+             .Build();
+
+            ITrigger trigger = TriggerBuilder.Create()
+            .WithIdentity("trigger1", "group1")
+            .WithCronSchedule("0 37 11 ? * *")
+            .Build();
+
+            sched.ScheduleJob(job, trigger);
+        }
+    }
+}
---
+++ src/SchedulerTask/SchedulerJob.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Quartz;
+
+namespace SchedulerTask
+{
+    class SchedulerJob: IJob
+    {
+        public void Execute(IJobExecutionContext context)
+        {
+            Console.WriteLine(DateTime.Now);
+        }
+    }
+}
