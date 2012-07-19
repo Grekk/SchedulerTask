f9d3a01110b9b6dc0171e0f43522eb7971383eb8
Gregory Maynard <gregory.maynard@valtech.fr>
2012-07-19 17:54:04 +0200

    Test

--- src/SchedulerTask/Program.cs
+++ src/SchedulerTask/Program.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using Quartz;
 using Quartz.Impl;
+using System.Threading;
+using SchedulerTask.MailService;
 
 namespace SchedulerTask
 {
@@ -11,26 +14,49 @@ namespace SchedulerTask
     {
         static void Main(string[] args)
         {
+            bool isMailSent = false;
+            while (Thread.CurrentThread.IsAlive)
+            {
+                if (DateTime.Today.DayOfWeek == DayOfWeek.Thursday)
+                {
+                    if (!isMailSent)
+                    {
+                        MailServiceClient client = new MailServiceClient();
+                        client.SendMail(ConfigurationManager.AppSettings["MAILING_LIST"], "<html><body>Test</body><html>");
+                        isMailSent = true;
+                    }
+                    else
+                    {
+                        Thread.Sleep(43200000);
+                    }
+                }
+                else
+                {
+                    isMailSent = false;
+                    Thread.Sleep(43200000);
+                }
+            }
+
             //SchedulerJob test = new SchedulerJob();
             //test.Execute(null);
 
-            // construct a scheduler factory
-            ISchedulerFactory schedFact = new StdSchedulerFactory();
+            //// construct a scheduler factory
+            //ISchedulerFactory schedFact = new StdSchedulerFactory();
 
-            // get a scheduler
-            IScheduler sched = schedFact.GetScheduler();
-            sched.Start();
+            //// get a scheduler
+            //IScheduler sched = schedFact.GetScheduler();
+            //sched.Start();
 
-            IJobDetail job = JobBuilder.Create<SchedulerJob>()
-             .WithIdentity("job1", "group1")
-             .Build();
+            //IJobDetail job = JobBuilder.Create<SchedulerJob>()
+            // .WithIdentity("job1", "group1")
+            // .Build();
 
-            ITrigger trigger = TriggerBuilder.Create()
-            .WithIdentity("trigger1", "group1")
-            .WithCronSchedule("0 45 17 ? * *")
-            .Build();
+            //ITrigger trigger = TriggerBuilder.Create()
+            //.WithIdentity("trigger1", "group1")
+            //.WithCronSchedule("0 45 17 ? * *")
+            //.Build();
 
-            sched.ScheduleJob(job, trigger);
+            //sched.ScheduleJob(job, trigger);
         }
     }
 }
